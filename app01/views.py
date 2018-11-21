b5ce179ea9d42be3c225180183a19cd215c4388c
kangle0224 <kangle0224@gmail.com>
2018-11-21 22:49:58 +0800

    modify index.html

--- app01/views.py
+++ app01/views.py
@@ -4,7 +4,11 @@ def index(request):
     return render(request, 'index.html')
 
 def ajax1(request):
+    print(request.GET)
+    print(request.POST)
     return HttpResponse("ajax1")
 
 def ajax2(request):
+    print(request.GET)
+    print(request.POST)
     return HttpResponse("ajax2")
